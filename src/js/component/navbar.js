4c78e19b84ba9dc7d0fe2c3279f76d75559b5afe
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-11-20 01:09:35 +0000

    Prroject star wars whit dinamilike icon and evaluates if the name already exist inside favorites prop

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css"
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+	const removeFromFavorites = (nameToRemove) => {
+		const updatedFavorites = store.favorites.filter(name => name !== nameToRemove);
+		actions.updateFavorites(updatedFavorites)
+	}
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div className="header">
+			<nav className="navbar">
+
+				<div className="left">
+
+					<Link to="/">
+						<img src="https://yt3.googleusercontent.com/0FgTOtFlh29rnwRZ0MUXAkkf-aho_LMsy2NJZNuoJhrb9UayXizmhn97gQb4_tWmiLOPSsZzE2I=s900-c-k-c0x00ffffff-no-rj" className="logo" alt=""></img>
+					</Link>
+
+				</div>
+
+				<div className="right">
+
+					<div className="ml-auto">
+						<div className="dropdown dropstart">
+							<button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+								Favorites
+							</button>
+							<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+								{store.favorites && store.favorites.length > 0 ? store.favorites.map((name, index) => {
+									return (
+										<li className="d-flex align-items-baseline" key={index}>
+											<p className="dropdown-item px-3" href="#">{name}</p>
+											<i onClick={() => removeFromFavorites(name)} className="fa-solid fa-trash px-3" style={{ color: "#ff000d" }}></i>
+										</li>
+									)
+								})
+									:
+									<li className="d-flex align-items-baseline">
+										<p className="dropdown-item px-3" href="#">Not favorite</p>
+
+									</li>
+								}
+
+							</ul>
+						</div>
+
+					</div>
+				</div>
+
+			</nav>
+
+		</div>
 	);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/js/layout.js
+++ src/js/layout.js
@@ -2,15 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
+import  Home  from "./views/home";
 import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { SingleCharacter } from "./views/singleCharacter";
+import { SinglePlanet } from "./views/singlePlanet";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-
-import CallCard from "./views/CallCard.js";
 
 //create your first component
 const Layout = () => {
@@ -26,8 +25,8 @@ const Layout = () => {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="/CallCard" element={ <CallCard/> }/>
+						<Route path="/singleCharacter/:uid" element={<SingleCharacter />} />
+						<Route path="/singlePlanet/:uid" element={<SinglePlanet />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
